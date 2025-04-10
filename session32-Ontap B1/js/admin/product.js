c7c8bb712d7ec418df5e7a488d70295ec8965730
hunghx <baby10051999@gmail.com>
2025-04-10 10:52:36 +0700

    valiate

--- session32-Ontap B1/js/admin/product.js
+++ session32-Ontap B1/js/admin/product.js
@@ -9,8 +9,21 @@ const USDollar = new Intl.NumberFormat('en-US', {
     currency: 'USD',
 });
 // làm tính năng 
+let countProductPerPage = 1;
+let currentPage = 1;
 
-const displayListProduct = () => {
+let totalPages = Math.ceil(products.length / countProductPerPage);
+
+const paginationProduct = (page) => {
+    let startIndex = page * countProductPerPage;
+    let endIndex = startIndex + countProductPerPage;
+    return products.slice(startIndex, endIndex);
+}
+
+
+
+
+const displayListProduct = (array) => {
     // lặp qua lần lượt từng sp va nối thành 1 chuối
     // let html = "";
     // products.forEach((pro,index)=>{
@@ -27,7 +40,7 @@ const displayListProduct = () => {
     //                     <td><button class="btn btn-danger"><i class="fa-solid fa-trash-xmark"></i></button></td>
     //                   </tr>`;
     // })
-    let html = products.reduce((temp, pro, index) => temp + `<tr>
+    let html = array.reduce((temp, pro, index) => temp + `<tr>
                 <th scope="row">${index + 1}</th>
                 <td>${pro.id}</td>
                 <td>${pro.name}</td>
@@ -47,9 +60,55 @@ const displayListProduct = () => {
     if (tbody) {
         tbody.innerHTML = html;
     }
+    /**
+     <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+    */
+
+    // hiển thị chân trang
+    
+    let pageHTML = new Array(totalPages).fill(1)
+        .reduce((temp, _, index) => temp + `<li class="page-item ${currentPage == index ? 'active' : ''}" onclick="goToPage(${index})"><a class="page-link" href="#">${index + 1}</a></li>`, "")
+    pageHTML = ` <li class="page-item ${currentPage == 0?'disabled':''}" onclick="prevPage()">
+            <a class="page-link">Previous</a>
+          </li>
+         ${pageHTML}
+          <li class="page-item ${currentPage == totalPages-1?'disabled':''}" onclick="nextPage()">
+            <a class="page-link" href="#">Next</a>
+          </li>`;
+    document.getElementById("list-pagination").innerHTML = pageHTML;
 }
 // gọi và thực thi hàm
-displayListProduct();
+displayListProduct(paginationProduct(currentPage));
+
+// Tạo hàm di chuyển trang khi onl\click
+const goToPage=(page)=>{
+    currentPage = page;
+    displayListProduct(paginationProduct(currentPage));
+}
+
+const nextPage=()=>{
+    if(currentPage<totalPages-1){
+    currentPage++;
+    displayListProduct(paginationProduct(currentPage));
+    }
+}
+const prevPage=()=>{
+    if(currentPage>0){
+    currentPage--;
+    displayListProduct(paginationProduct(currentPage));
+    }
+}
+
 
 
 
@@ -64,7 +123,7 @@ const deleteProductById = (id) => {
     // lưu vào storage :
     localStorage.setItem("products", JSON.stringify(products));
     // hiển thị lại danh sách sau khi xóa
-    displayListProduct();
+    displayListProduct(products);
 }
 
 
@@ -80,15 +139,15 @@ const handleAddProduct = () => {
     if (product) {
         // thành công nhưng sẽ load lại trang và tải lại dữu liêu : Browser Storage
         products.push(product);
-        // displayListProduct();
-        // let myModal = document.getElementById('modal-add');
-        // let modal = bootstrap.Modal.getInstance(myModal)
-        // modal.hide();
+        displayListProduct(products);
+        let myModal = document.getElementById('modal-add');
+        let modal = bootstrap.Modal.getInstance(myModal)
+        modal.hide();
 
         // lưu vào storage :
-        localStorage.setItem("products", JSON.stringify(products));
+        // localStorage.setItem("products", JSON.stringify(products));
 
-        location.href = "/admin/product/list-product.html";
+        // location.href = "/admin/product/list-product.html";
 
     }
 
@@ -118,6 +177,36 @@ const validateInputProduct = () => {
 
     return flag ? { id, name, description, price, imgUrl, stock } : null;
 }
-const showFormAdd=()=>{
-    document.getElementById("modal-add").style.display = "block";
+
+const searchProduct = () => {
+    // lấy giá trị người dùng nhập vào
+    let keyword = document.getElementById("keyword").value.toLocaleLowerCase();
+    // lọc dữ liệu
+    let filterArray = products.filter(pro => pro.name.toLocaleLowerCase().includes(keyword) || pro.description.toLocaleLowerCase().includes(keyword))
+    // in ra màn hình kết quả 
+    displayListProduct(filterArray);
 }
+
+// Phân trang:
+// Thành phần : số lượng phần tử trên trang : productCountPerPage , Trang hiện tại : currentPage
+
+// totalElement = 30 ;
+// hiển thị 6 phần tử trên trang : hiển thị trang số 1 : index Start: 0 -> index end: 5
+// hiển thị 6 phần tử trên trang : hiển thị trang số 2 : index Start: 6 -> index end: 11
+// hiển thị 6 phần tử trên trang : hiển thị trang số 3 : index Start: 12 -> index end: 17
+// hiển thị 6 phần tử trên trang : hiển thị trang số 4 : index Start: 18 -> index end: 23
+// mối liện hệ giữa vị trí và số trang và số phần tử trên 1 trang
+// start = (currentPage-1)*productCountPerPage
+// end = start + productCountPerPage;
+
+// slice(start, end);
+
+// tính tổng số trang :
+// totalPages : 30 / 5 => 6 Math.cell
+
+
+
+
+// const showFormAdd=()=>{
+//     document.getElementById("modal-add").style.display = "block";
+// }
